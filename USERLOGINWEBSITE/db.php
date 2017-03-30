3d29265cb7b30f94a53721ae6331c0b19257d9b6
BuildTools <unconfigured@null.spigotmc.org>
2017-03-30 14:46:58 +0100

    test
    test

---
+++ USERLOGINWEBSITE/db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'csmysql.cs.cf.ac.uk';
+$user = 'c1639991';
+$pass = 'deopadjek9';
+$db = 'c1639991';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
